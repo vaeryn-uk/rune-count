4c9c5fffa7aca20463b7beb2fa5bb078407ea05a
Andy Cook <andrewcook1990@gmail.com>
2026-07-25 23:13:02 +0200

    Rename RuneLite development launcher

--- src/main/java/com/runecount/RuneCountDevLauncher.java
+++ src/main/java/com/runecount/RuneCountDevLauncher.java
@@ -4,7 +4,7 @@ import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
 /** Launches RuneLite with Rune Count registered as a built-in development plugin. */
-public class RuneCountPluginTest
+public class RuneCountDevLauncher
 {
 	public static void main(String[] args) throws Exception
 	{
